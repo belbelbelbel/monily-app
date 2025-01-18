c23da51f47b67a4514818a4372bc8639dfca9db6
belbelbelbel <gronaldchia@gmail.com>
2025-01-18 20:05:25 +0100

    resolved commits

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
   }, []);
   return (
     <div  className='bg-bg-img-1   xl:h-[100vh]  md:h-[90vh] lg:h-[92vh] h-[90vh]   bg-black  bg-cente  md:bg-cover text-white bg-no-repeat'>
-      <div className='w-full md:h-[90vh] lg:h-[92vh]   xl:h-[100vh] h-[90vh] z-30 absolute md:bg-opacity-60 bg-opacity-80 bg-black'></div>
+      <div className='w-full md:h-[90vh] lg:h-[92vh]   xl:h-[100vh] h-[90vh] z-30 absolute md:bg-opacity-60 bg-opacity-70 bg-black'></div>
       <div className='relative flex   h-full flex-col'>
         <div className='relative'>
           <NavBar />
