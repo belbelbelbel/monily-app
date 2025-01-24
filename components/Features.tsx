559d3396aaa73270916f3dc6f67b8a78ac9f4f3b
belbelbelbel <gronaldchia@gmail.com>
2025-01-25 00:17:58 +0100

    reconciled commits

--- components/Features.tsx
+++ components/Features.tsx
@@ -21,16 +21,16 @@ const Features = () => {
     }, []);
 
     return (
-        <div className={`w-full bg-black md:h-[80%]   lg:h-[40rem] xl:h-[60%] h-[100%] items-center flex justify-center ${sans.className}`}>
-            <div className='flex  lg:flex-row flex-col items-center gap-4 bg-black h-[100%] justify-center w-[90%] mx-auto'>
+        <div className={`w-full bg-black md:h-[80%]   lg:h-[76rem] xl:h-[60%] h-[100%] items-center flex justify-center ${sans.className}`}>
+            <div className='flex  xl:flex-row flex-col items-center gap-4 bg-black h-[100%] justify-center w-[90%] mx-auto'>
                 {
                     FaeturesArray.map((feature, index) => (
-                        <div key={index} className={`flex lg:h-[18rem] md:h-[21.5rem] lt:h-[17.7rem] 3xl:h-[40%] xl:h-[53%] h-[30%] py-4 text-center text-transparent bg-gradient-to-l from-[#1A1B23] via-black to-[#006270] shadow  justify-center lt:px-10 px-6 rounded-[20px] shadow border-2xl  m-4 flex-col items-center gap-2 lt:gap-5 md:gap-4 w-[80%] lt:w-[70%] md:w-[65%] 3xl:w-[30%] xl:w-[27%]`}>
+                        <div key={index} className={`flex lg:h-[38rem] md:h-[21.5rem] lt:h-[17.7rem] 3xl:h-[40%] xl:h-[53%] h-[30%] py-4 text-center text-transparent bg-gradient-to-l from-[#1A1B23] via-black to-[#006270] shadow  justify-center lt:px-10 px-6 rounded-[20px] shadow border-2xl  m-4 flex-col items-center gap-2 lt:gap-5 md:gap-4 w-[80%] lt:w-[70%] md:w-[65%] 3xl:w-[30%] xl:w-[27%]`}>
                             <div className="md:w-16 md:h-16 w-12 h-12 md:bg-gradient-to-b from-gray-800 via-gray-600 to-gray-800 flex flex-col items-center justify-center rounded-full bg-black ">
-                                <feature.icon className='md:text-3xl lt:text-[9rem]  text-2xl  text-white bg-gradient-to-r from-[#006270] via-gray-300 to-blue-500 bg-clip-text animate-shine ' />
+                                <feature.icon className='md:text-3xl xl:text-3xl lg:text-5xl lt:text-[9rem]  text-2xl  text-white bg-gradient-to-r from-[#006270] via-gray-300 to-blue-500 bg-clip-text animate-shine ' />
                             </div>
-                            <div  data-aos="zoom-out" className='text-white lg:text-2xl xl:text-[1.5rem] 3xl:text-[1.8rem]  lt:text-[2.3rem]  text-xl font-bold '>{feature.title}</div>
-                            <div className='text-white lg:text-[0.8rem] xl:text-[0.9rem] 3xl:text-[1rem] lt:text-[1.35rem] text-[0.8rem] w-full'>{feature.description}</div>
+                            <div  data-aos="zoom-out" className='text-white lg:text-3xl xl:text-[1.5rem] 3xl:text-[1.8rem]  lt:text-[2.3rem]  text-xl font-bold '>{feature.title}</div>
+                            <div className='text-white lg:text-[1.5rem] xl:text-[0.9rem] 3xl:text-[1rem] lt:text-[1.35rem] text-[0.8rem] w-full'>{feature.description}</div>
                         </div>
                     ))
                 }
